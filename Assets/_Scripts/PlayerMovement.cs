5802842ee727ceb6f52827c4acc03d4fb2aaad96
Mikhail <mikhail.eremeev.work@gmail.com>
2023-01-25 21:20:25 +0300

    Added bysic graphic and bysic gameplay

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Transform capsuleTransform;
+    [SerializeField] private float limitValue;
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            MovePlayer();
+        }
+    }
+
+    private void MovePlayer()
+    {
+        float halfScreen = Screen.width / 2;
+        float xPos = (Input.mousePosition.x - halfScreen) / halfScreen;
+        float finalXPos = Mathf.Clamp(xPos * limitValue, -limitValue, limitValue);
+
+        capsuleTransform.localPosition = new Vector3(finalXPos, 1, 0);
+    }
+}
